5fa916ad6791db12d04f0bdcc0351c1005850be9
Вадим <vshhukin@mail.ru>
2018-04-17 23:25:01 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
-// Slick slider settings:
-$(function() {
-  $('.slider').slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-    draggable: true,
-    pauseOnFocus: false,
-    pauseOnHover: false,
-    speed: 1000,
-    dots: true
-  });
+$(document).ready(function () {
 
-  $('.slider-top').slick({
-    slidesToShow: 1,
-    arrows: false,
-    asNavFor: '.slider-bottom'
-  });
-
-  $('.slider-bottom').slick({
-    slidesToShow: 3,
-    arrows: false,
-    asNavFor: '.slider-top',
-    focusOnSelect: true
-  });
 });
+
